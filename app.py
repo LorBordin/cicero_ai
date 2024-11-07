c56bb4a7acc3014a0c025681ca0dee0e8e749ad8
Lorenzo Bordin <lorenzo.bordin@192.168.1.60>
2024-11-07 09:06:32 +0100

    changed secrets managment

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ if __name__ == "__main__":
         llm_client = LLMClient(
             client_name="groq",
             model="llama3-70b-8192",
-            api_key=os.getenv("GROQ_API_KEY")
+            api_key=st.secrets["GROQ_API_KEY"]
         )
 
     main()
